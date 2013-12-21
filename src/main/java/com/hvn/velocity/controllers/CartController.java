a5d605357e89de976a9a11f14cd49dd6f5b72dc7
sunshine55 <tony.wu90@yahoo.com>
2013-12-21 10:31:34 +0700

    update shopping cart

--- src/main/java/com/hvn/velocity/controllers/CartController.java
+++ src/main/java/com/hvn/velocity/controllers/CartController.java
@@ -1,21 +1,18 @@
 package com.hvn.velocity.controllers;
 
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.hvn.velocity.entities.Cart;
 import com.hvn.velocity.entities.Product;
 import com.hvn.velocity.services.ProductService;
+import com.hvn.velocity.session.Cart;
 
 @Controller
 public class CartController {
@@ -29,14 +26,7 @@ public class CartController {
 	@RequestMapping(value = "/cart", method = RequestMethod.GET)
 	public String cart(ModelMap mm) {
 		Map<Product, Integer> itemMap = cart.getProducts();
-		double subTotal = 0;
-		if (itemMap.size() != 0) {
-			Set<Entry<Product, Integer>> entries = itemMap.entrySet();
-			for (Entry<Product, Integer> entry : entries) {
-				subTotal += entry.getKey().getPrice().doubleValue()
-						* entry.getValue();
-			}
-		}
+		double subTotal = cart.calculateSubTotal();
 		mm.put("itemMap", itemMap);
 		mm.put("subTotal", subTotal);
 		return "cart";
@@ -57,10 +47,9 @@ public class CartController {
 	}
 
 	@RequestMapping(value = "/updateCart", method = RequestMethod.POST)
-	public String updateCart(@ModelAttribute("id") Integer id,
-			@ModelAttribute("quantity") Integer quantity) {
-		Product product = productService.getById(id);
-		cart.updateProduct(product, quantity);
+	public String updateCart(@RequestParam("id") Integer id,
+			@RequestParam("quantity") Integer quantity) {
+		cart.updateProduct(id, quantity);
 		return "redirect:/cart";
 	}
 
---
+++ src/main/java/com/hvn/velocity/session/Cart.java
@@ -0,0 +1,70 @@
+package com.hvn.velocity.session;
+
+import java.text.DecimalFormat;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import com.hvn.velocity.entities.Product;
+
+public class Cart {
+
+	private Map<Product, Integer> products = new HashMap<Product, Integer>();
+
+	public Map<Product, Integer> getProducts() {
+		return Collections.unmodifiableMap(this.products);
+	}
+
+	public void addProduct(Product product) {
+		if (!products.isEmpty()) {
+			Set<Product> keys = products.keySet();
+			int i = 0;
+			for (Product key : keys) {
+				if (key.getId() == product.getId()) {
+					int quantity = products.get(key);
+					quantity++;
+					products.put(key, quantity);
+					break;
+				}
+				i++;
+				if (i >= keys.size()) {
+					products.put(product, 1);
+				}
+			}
+		} else {
+			products.put(product, 1);
+		}
+	}
+
+	public void clear() {
+		this.products.clear();
+	}
+
+	public void updateProduct(Integer id, Integer qty) {
+		Set<Product> keys = products.keySet();
+		for (Product key : keys) {
+			if (key.getId() == id) {
+				if (qty <= 0)
+					products.remove(key);
+				else
+					products.put(key, qty);
+				break;
+			}
+		}
+	}
+
+	public double calculateSubTotal() {
+		double sum = 0;
+		if (products.size() != 0) {
+			Set<Entry<Product, Integer>> entries = products.entrySet();
+			for (Entry<Product, Integer> entry : entries) {
+				sum += entry.getKey().getPrice().doubleValue()
+						* entry.getValue();
+			}
+		}
+		DecimalFormat form = new DecimalFormat("#.##");
+		return Double.valueOf(form.format(sum));
+	}
+}
