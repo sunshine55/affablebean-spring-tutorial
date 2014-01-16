768bd58002b0ac810563ad8a4e845d524756b1ca
sunshine55 <tony.wu90@yahoo.com>
2014-01-16 16:41:06 +0700

    admin cosole: prepare views and controller

---
+++ src/main/webapp/resources/js/sb-admin.js
@@ -0,0 +1,31 @@
+$(function() {
+
+    $('#side-menu').metisMenu();
+    $('#dataTables-example').dataTable();
+    
+});
+
+//Loads the correct sidebar on window load
+$(function() {
+
+    $(window).bind("load", function() {
+        if ($(this).width() < 768) {
+            $('div.sidebar-collapse').addClass('collapse');
+        } else {
+            $('div.sidebar-collapse').removeClass('collapse');
+        }
+    });
+});
+
+//Collapses the sidebar on window resize
+$(function() {
+
+    $(window).bind("resize", function() {
+        console.log($(this).width());
+        if ($(this).width() < 768) {
+            $('div.sidebar-collapse').addClass('collapse');
+        } else {
+            $('div.sidebar-collapse').removeClass('collapse');
+        }
+    });
+});
