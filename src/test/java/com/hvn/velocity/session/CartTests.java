200fc588350ad5ed4ad9a72b9073d0f905871e55
sunshine55 <tony.wu90@yahoo.com>
2014-04-30 16:13:16 +0700

    unit test for Cart

--- src/test/java/com/hvn/velocity/session/CartTests.java
+++ src/test/java/com/hvn/velocity/session/CartTests.java
@@ -2,53 +2,83 @@ package com.hvn.velocity.session;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.hvn.velocity.domain.Product;
+
 public class CartTests {
-
+	
+	private Cart cart;
+	private ArrayList<Product> testData;
+	
 	@Before
-	public void setUp() throws Exception {
+	public void setup() throws Exception {
+		Product prod1 = new Product(null, "Product_1", new BigDecimal("2.5"), null);
+		prod1.setId(1);
+		Product prod2 = new Product(null, "Product_2", new BigDecimal("3"), null);
+		prod2.setId(2);
+		
+		testData = new ArrayList<Product>();
+		testData.add(prod1);
+		testData.add(prod2);
+		
+		cart = new Cart();
+		for (int i = 0; i < 2; i++) {
+			cart.addItem(testData.get(0));
+		}
+		cart.addItem(testData.get(1));
 	}
-
+	
 	@After
-	public void tearDown() throws Exception {
+	public void teardown() throws Exception {
+		cart = null;
+		testData.clear();
 	}
 
 	@Test
 	public void testGetItems() {
-		fail("Not yet implemented");
+		assertThat(cart.getItems()).hasSize(2);
 	}
 
 	@Test
-	public void testAddItem() {
-		fail("Not yet implemented");
+	public void testAddItems() {
+		assertThat(cart.getItems()).containsKeys(testData.get(0), testData.get(1));
+		assertThat(cart.getItems().get(testData.get(0))).isEqualTo(2);
+		assertThat(cart.getItems().get(testData.get(1))).isEqualTo(1);
 	}
 
 	@Test
 	public void testClear() {
-		fail("Not yet implemented");
+		cart.clear();
+		assertThat(cart.getItems()).hasSize(0);
 	}
 
 	@Test
 	public void testUpdateItem() {
-		fail("Not yet implemented");
+		cart.updateItem(2, 2);
+		assertThat(cart.getItems()).containsKeys(testData.get(0), testData.get(1));
+		assertThat(cart.getItems().get(testData.get(0))).isEqualTo(2);
+		assertThat(cart.getItems().get(testData.get(1))).isEqualTo(2);
 	}
 
 	@Test
 	public void testCalculateSubTotal() {
-		fail("Not yet implemented");
+		assertThat(cart.calculateSubTotal()).isEqualTo(8.00);
 	}
 
 	@Test
 	public void testCalculateTotal() {
-		fail("Not yet implemented");
+		assertThat(cart.calculateTotal(8.00)).isEqualTo(11.00);
 	}
 
 	@Test
 	public void testSumQuantity() {
-		fail("Not yet implemented");
+		assertThat(cart.sumQuantity()).isEqualTo(3);
 	}
 
 }
