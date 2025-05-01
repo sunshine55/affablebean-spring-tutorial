4087bf9da7b5407042aecfb9bd8054d54dec43f7
sunshine55 <tony.wu90@yahoo.com>
2025-04-30 21:56:07 -0500

    split documents category and item; implement CRUD for each one

--- afbb-gradle/api/src/main/java/sunshine55/tutorial/afbb/api/dto/ItemDto.java
+++
@@ -1,9 +0,0 @@
-package sunshine55.tutorial.afbb.api.dto;
-
-import lombok.Data;
-
-@Data
-public class ItemDto {
-    private String name, description;
-    private Float price;
-}
--- afbb-gradle/api/src/main/java/sunshine55/tutorial/afbb/api/entity/ItemEntity.java
+++ afbb-gradle/api/src/main/java/sunshine55/tutorial/afbb/api/entity/ItemEntity.java
@@ -7,16 +7,18 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-@Document(collection = "category")
+@Document(collection = "item")
 @Getter @Setter
 @EqualsAndHashCode
-public class CategoryEntity {
+public class ItemEntity {
     @Id private String id;
 
-    private String description, name;
-    
-    public void modifyBy(CategoryEntity category) {
-        this.description = category.getDescription();
-        this.name = category.getName();
+    private String categoryId, description, name;
+    private Double price;
+
+    public void modifyBy(ItemEntity item) {
+        this.description = item.description;
+        this.name = item.name;
+        this.price = item.price;
     }
 }
