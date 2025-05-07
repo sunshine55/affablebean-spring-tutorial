39419a9cc26aee5becd793c4976318906d8e4999
sunshine55 <tony.wu90@yahoo.com>
2025-05-06 23:01:58 -0500

    scaffold initial layout for admin gui

---
+++ afbb-gui/admin/src/app/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Header: React.FC = () => {
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <h1 className="text-xl font-bold">Affable Spring Bean</h1>
+    </div>
+  );
+};
