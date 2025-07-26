82fa7a228f439410300378c85e83a9c3234a0c88
sunshine55 <tony.wu90@yahoo.com>
2025-07-25 21:30:23 -0500

    rewrite CategoryControllerTest in Micronaut manner

--- afbb-ws/src/test/java/sunshine55/tutorial/afbb/ws/controller/CategoryControllerTest.java
+++ afbb-ws/src/test/java/sunshine55/tutorial/afbb/ws/controller/CategoryControllerTest.java
@@ -3,118 +3,126 @@ package sunshine55.tutorial.afbb.ws.controller;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 
+import io.micronaut.core.io.ResourceLoader;
+import io.micronaut.core.type.Argument;
+import io.micronaut.serde.ObjectMapper;
+import io.micronaut.test.annotation.MockBean;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
 import sunshine55.tutorial.afbb.ws.dao.CategoryDao;
 import sunshine55.tutorial.afbb.ws.entity.CategoryEntity;
 import sunshine55.tutorial.afbb.ws.service.InstanceCreator;
 
-@ExtendWith(MockitoExtension.class)
+@MicronautTest
 public class CategoryControllerTest {
-    @Mock private CategoryDao categoryDao;
-    @Mock private InstanceCreator instanceCreator;
-
-    @InjectMocks private CategoryController categoryController;
+    // Dependencies
+    @Inject CategoryDao categoryDao;
+    @Inject InstanceCreator instanceCreator;
+    // Mocking dependencies
+    @MockBean(CategoryDao.class)
+    CategoryDao categoryDao() {
+        return mock(CategoryDao.class);
+    }
+    @MockBean(InstanceCreator.class)
+    InstanceCreator instanceCreator() {
+        return mock(InstanceCreator.class);
+    }
+    // Class under test
+    @Inject CategoryController categoryController;
 
     @Nested
     public class GetTest {
 
         @Test
         @DisplayName("Get all categories")
-        void test1() {
-            CategoryEntity cat1 = new CategoryEntity();
-            CategoryEntity cat2 = new CategoryEntity();
-            when(categoryDao.findAll()).thenReturn(List.of(cat1, cat2));
-            List<CategoryEntity> categories = categoryController.get(null);
-            assertEquals(2, categories.size());
-            assertSame(cat1, categories.getFirst());
-            assertSame(cat2, categories.getLast());
+        public void test1() {
+            List<CategoryEntity> categories = new ArrayList<>();
+            categories.add(new CategoryEntity());
+            when(categoryDao.findAll()).thenReturn(categories);
+            List<CategoryEntity> result = categoryController.get(null);
+            assertSame(categories, result);
         }
 
         @Test
-        @DisplayName("Get category by id - found")
-        void test2() {
-            CategoryEntity cat = new CategoryEntity();
-            when(categoryDao.findById("abc")).thenReturn(Optional.of(cat));
-            List<CategoryEntity> resultList = categoryController.get("abc");
-            assertEquals(1, resultList.size());
-            assertSame(cat, resultList.getFirst());
+        @DisplayName("Get category by id (found)")
+        public void test2() {
+            CategoryEntity found = new CategoryEntity();
+            when(categoryDao.findById("abc")).thenReturn(Optional.of(found));
+            List<CategoryEntity> result = categoryController.get("abc");
+            assertEquals(1, result.size());
+            assertEquals(found, result.getFirst());
         }
 
         @Test
-        @DisplayName("Get category by id - not found")
-        void test3() {
-            when(categoryDao.findById("xyz")).thenReturn(Optional.empty());
-            List<CategoryEntity> resultList = categoryController.get("xyz");
-            assertTrue(resultList.isEmpty());
+        @DisplayName("Get category by id (not found)")
+        public void test3() {
+            when(categoryDao.findById("abc")).thenReturn(Optional.empty());
+            List<CategoryEntity> result = categoryController.get("abc");
+            assertTrue(result.isEmpty());
         }
     }
 
     @Nested
     public class UpsertTest {
+        @Inject ResourceLoader resourceLoader;
+        @Inject ObjectMapper objectMapper;
 
-        @Test
-        @DisplayName("Upsert new categories (no id)")
-        void test1() {
-            CategoryEntity inputCat = new CategoryEntity();
-            CategoryEntity newCat = new CategoryEntity();
-            CategoryEntity savedCat = new CategoryEntity();
-            
-            when(instanceCreator.initCategory()).thenReturn(newCat);
-            when(categoryDao.saveAll(List.of(newCat))).thenReturn(List.of(savedCat));
+        private List<CategoryEntity> existingCategories;
 
-            List<CategoryEntity> result = categoryController.upsert(List.of(inputCat));
-            assertEquals(1, result.size());
-            assertEquals(newCat, savedCat);
+        @BeforeEach
+        public void setUp() throws IOException {
+            InputStream inputStream = resourceLoader
+                .getResourceAsStream("existingCategories.json")
+                .orElse(null);
+            existingCategories = objectMapper
+                .readValue(inputStream, Argument.listOf(CategoryEntity.class));
         }
 
         @Test
-        @DisplayName("Upsert existing category (with id, found)")
-        void test2() {
-            CategoryEntity inputCat = new CategoryEntity();
-            inputCat.setId("id1");
-            CategoryEntity existingCat = new CategoryEntity();
-            existingCat.setId("id1");
-            CategoryEntity savedCat = new CategoryEntity();
-            savedCat.setId("id1");
-
-            when(categoryDao.findById("id1")).thenReturn(Optional.of(existingCat));
-            when(categoryDao.saveAll(List.of(existingCat))).thenReturn(List.of(savedCat));
-
-            List<CategoryEntity> result = categoryController.upsert(List.of(inputCat));
-            assertEquals(1, result.size());
-            assertEquals("id1", result.getFirst().getId());
+        @DisplayName("Insert categories")
+        public void test1() {
+            // all new categories
+            existingCategories.forEach(category -> category.setId(null));
+            // second category id is not found
+            existingCategories.getFirst().setId("some-id");
+            when(categoryDao.findById("some-id")).thenReturn(Optional.empty());
+            when(instanceCreator.initCategory()).thenReturn(new CategoryEntity());
+            // execute test and verify
+            when(categoryDao.saveAll(anyList())).thenReturn(existingCategories);
+            List<CategoryEntity> result = categoryController.upsert(existingCategories);
+            assertEquals(4, result.size());
+            verify(categoryDao).saveAll(anyList());
+            verify(categoryDao, times(0)).updateAll(anyList());
         }
 
         @Test
-        @DisplayName("Upsert category (with id, not found)")
-        void test3() {
-            CategoryEntity inputCat = new CategoryEntity();
-            inputCat.setId("id2");
-            CategoryEntity newCat = new CategoryEntity();
-            newCat.setId("id2");
-            CategoryEntity savedCat = new CategoryEntity();
-            savedCat.setId("id2");
-
-            when(categoryDao.findById("id2")).thenReturn(Optional.empty());
-            when(instanceCreator.initCategory()).thenReturn(newCat);
-            when(categoryDao.saveAll(List.of(newCat))).thenReturn(List.of(savedCat));
-
-            List<CategoryEntity> result = categoryController.upsert(List.of(inputCat));
-            assertEquals(1, result.size());
-            assertEquals("id2", result.getFirst().getId());
+        @DisplayName("Update categories")
+        public void test2() {
+            existingCategories.forEach(category -> {
+                when(categoryDao.findById(category.getId())).thenReturn(Optional.of(category));
+            });
+            when(categoryDao.updateAll(anyList())).thenReturn(existingCategories);
+            List<CategoryEntity> result = categoryController.upsert(existingCategories);
+            assertEquals(4, result.size());
+            verify(categoryDao, times(0)).saveAll(anyList());
+            verify(categoryDao).updateAll(anyList());
         }
     }
 
@@ -123,15 +131,14 @@ public class CategoryControllerTest {
 
         @Test
         @DisplayName("Delete all categories")
-        void test1() {
+        public void test1() {
             categoryController.delete(null);
-            // Verify that deleteAll is called
             verify(categoryDao).deleteAll();
         }
 
         @Test
         @DisplayName("Delete category by id")
-        void test2() {
+        public void test2() {
             categoryController.delete("abc");
             verify(categoryDao).deleteById("abc");
         }
