b31aeb94fa74198c728063470d3d7489f489aafd
sunshine55 <tony.wu90@yahoo.com>
2025-08-04 22:36:17 -0500

    afbb-gui: admin - edit/delete/create item

--- afbb-gui/admin/app/categories/[categoryId]/items/[itemId]/page.tsx
+++ afbb-gui/admin/app/categories/[categoryId]/items/[itemId]/page.tsx
@@ -1,5 +1,29 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+import { useParams } from 'next/navigation';
+
+import { itemSchema, ItemModel } from '@/schema';
+import { Spinner } from '@/components';
+import { ItemForm } from '@/ui';
+
 export default function ItemEditPage() {
-  return <h1>Work in Progress</h1>;
+  const { itemId } = useParams();
+
+  const [item, setItem] = useState<ItemModel>({} as ItemModel);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    if (!itemId) return;
+    fetch(`${process.env.NEXT_PUBLIC_AFBB_API}/items?id=${itemId}`)
+      .then((res) => res.json())
+      .then((data) => setItem(itemSchema.parse(data[0] || {})))
+      .finally(() => setLoading(false));
+  }, [itemId]);
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  return <ItemForm {...item} />;
 }
--- afbb-gui/admin/app/categories/[categoryId]/items/create/page.tsx
+++ afbb-gui/admin/app/categories/[categoryId]/items/create/page.tsx
@@ -1,5 +1,18 @@
 'use client';
 
+import { useParams } from 'next/navigation';
+import { ItemModel } from '@/schema';
+import { ItemForm } from '@/ui';
+
 export default function ItemCreatePage() {
-  return <h1>Work in Progress</h1>;
+  const { categoryId } = useParams();
+  const item: ItemModel = {
+    id: '',
+    name: '',
+    description: '',
+    imageSrc: '',
+    price: 0,
+    categoryId: categoryId as string,
+  };
+  return <ItemForm {...item} />;
 }
