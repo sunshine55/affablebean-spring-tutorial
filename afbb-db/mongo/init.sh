f9292e8b947cb8fc6c80c457bd62f6fe7afd709e
sunshine55 <tony.wu90@yahoo.com>
2026-08-12 19:00:37 -0500

    refine database setup

--- afbb-db/mongo/init.sh
+++ afbb-db/mongo/init.sh
@@ -1,3 +1,4 @@
-#!/bin/sh
+#!/bin/bash
+
 mongoimport --db afbb --collection category --drop --file /docker-entrypoint-initdb.d/category.json --jsonArray
 mongoimport --db afbb --collection item --drop --file /docker-entrypoint-initdb.d/item.json --jsonArray
