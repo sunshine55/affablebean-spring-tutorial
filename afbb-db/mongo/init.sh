a51feb4f8a9ba88e1d43938bb476b67f8d6e747c
sunshine55 <tony.wu90@yahoo.com>
2026-08-18 00:21:07 -0500

    fix query issue for admin gui
    - use init.js instead of sh due to objectId relation
    - combine all data seed into one entry

--- afbb-db/mongo/init.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mongoimport --db afbb --collection category --drop --file /docker-entrypoint-initdb.d/category.json --jsonArray
-mongoimport --db afbb --collection item --drop --file /docker-entrypoint-initdb.d/item.json --jsonArray
